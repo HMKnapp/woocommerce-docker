1b87127de24325247c38f8bc21488ad93c31141d
Herbert Knapp <42992894+HMKnapp@users.noreply.github.com>
2021-08-27 09:53:15 +0200

    Prepare for persistent installations

--- docker/wordpress/init.sh
+++ docker/wordpress/init.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [[ -z ${WORDPRESS_URL} ]]; then
+if [[ -z ${WORDPRESS_URL} && ! -e wp-config.php ]]; then
   echo "WORDPRESS_URL not specified."
   if [[ -n ${NGROK_TOKEN} ]]; then 
     echo "Launching ngrok to get temporary URL"
@@ -18,48 +18,69 @@ while ! mysqladmin ping -h"${WORDPRESS_DB_HOST}" --silent; do
   sleep 1
 done
 
-echo "Creating Database"
+function create_db() {
+  echo "Creating Database"
 
-wp config create \
-  --dbhost=${WORDPRESS_DB_HOST} \
-  --dbname=${WORDPRESS_DB_NAME} \
-  --dbuser=${WORDPRESS_DB_USER} \
-  --dbpass=${WORDPRESS_DB_PASS} \
-  --locale=${WORDPRESS_LOCALE}
+  wp config create \
+    --dbhost=${WORDPRESS_DB_HOST} \
+    --dbname=${WORDPRESS_DB_NAME} \
+    --dbuser=${WORDPRESS_DB_USER} \
+    --dbpass=${WORDPRESS_DB_PASS} \
+    --locale=${WORDPRESS_LOCALE}
+}
 
-echo "Installing Wordpress"
+function install_core() {
+  echo "Installing Wordpress"
 
-wp core install \
-  --url=${WORDPRESS_URL} \
-  --title=${WORDPRESS_TITLE} \
-  --admin_user=${WORDPRESS_ADMIN_USER} \
-  --admin_password=${WORDPRESS_ADMIN_PASS} \
-  --admin_email=${WORDPRESS_ADMIN_EMAIL} \
-  --skip-email
+  wp core install \
+    --url=${WORDPRESS_URL} \
+    --title=${WORDPRESS_TITLE} \
+    --admin_user=${WORDPRESS_ADMIN_USER} \
+    --admin_password=${WORDPRESS_ADMIN_PASS} \
+    --admin_email=${WORDPRESS_ADMIN_EMAIL} \
+    --skip-email
+}
 
-echo "Installing WooCommerce"
-wp plugin install woocommerce --activate
+function install_woocommerce() {
+  echo "Installing WooCommerce"
+  wp plugin install woocommerce --activate
 
-echo "Install Sample Data"
-wp plugin install wordpress-importer --activate
-wp import wp-content/plugins/woocommerce/sample-data/sample_products.xml --authors=create
+  echo "Install Sample Data"
+  wp plugin install wordpress-importer --activate
+  wp import wp-content/plugins/woocommerce/sample-data/sample_products.xml --authors=create
+}
 
-if [[ -n ${PLUGIN_URL} ]]; then
+function install_plugin() {
   STR_PLUGIN=$(get_plugin.sh ${PLUGIN_URL} ${PLUGIN_VERSION})
   PLUGIN_NAME=$(echo ${STR_PLUGIN} | cut -d'^' -f1)
   PATH_TO_ZIP=$(echo ${STR_PLUGIN} | cut -d'^' -f2)
-  wp plugin install ${PATH_TO_ZIP} --activate
-fi
+  wp plugin install ${PATH_TO_ZIP} --activate  
+}
 
-echo
-echo '####################################'
-echo
-echo "URL: https://${WORDPRESS_URL}"
-echo "Panel: https://${WORDPRESS_URL}/wp-admin/"
-echo "User: ${WORDPRESS_ADMIN_USER}"
-echo "Password: ${WORDPRESS_ADMIN_PASS}"
-echo
-echo '####################################'
-echo
+function print_info() {
+  echo
+  echo '####################################'
+  echo
+  echo "URL: https://${WORDPRESS_URL}"
+  echo "Panel: https://${WORDPRESS_URL}/wp-admin/"
+  echo "User: ${WORDPRESS_ADMIN_USER}"
+  echo "Password: ${WORDPRESS_ADMIN_PASS}"
+  echo
+  echo '####################################'
+  echo
+}
+
+if [[ -e wp-config.php ]]; then
+  echo "Wordpress detected. Skipping installations"
+  WORDPRESS_URL=$(wp option get siteurl | sed 's/http:/https:/')
+else
+  create_db
+  install_core
+  install_woocommerce
+  if [[ -n ${PLUGIN_URL} ]]; then
+    install_plugin
+  fi
+fi
+print_info
 
 apache2-foreground "$@"
